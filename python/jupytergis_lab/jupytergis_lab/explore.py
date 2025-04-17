72d4770807e97c02a9654129e721e557296fefd7
Matt Fisher <mfisher87@gmail.com>
2025-04-16 21:49:32 -0600

    Add fresh_doc parameter to explore function

--- python/jupytergis_lab/jupytergis_lab/explore.py
+++ python/jupytergis_lab/jupytergis_lab/explore.py
@@ -5,12 +5,21 @@ from sidecar import Sidecar
 from jupytergis_lab import GISDocument
 
 
-def explore(geojson_path: str | Path) -> None:
+def explore(
+    geojson_path: str | Path,
+    fresh_doc: bool = False,
+) -> None:
     """Run a JupyterGIS data interaction interface alongside a Notebook.
 
     :param geojson_path: Path to a GeoJSON file.
+    :param fresh_doc: Remove any pre-existing explore document before creating a new doc.
     """
-    doc = GISDocument("debug.jGIS")
+    filename = "explore.jGIS"
+
+    if fresh_doc:
+        Path(filename).unlink(missing_ok=True)
+
+    doc = GISDocument(filename)
 
     # TODO: Basemap choices
     doc.add_raster_layer(
