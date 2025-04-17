d1919c8aa19e6f613dc72c5b38e7b91db092ccde
Matt Fisher <mfisher87@gmail.com>
2025-04-16 20:02:18 -0600

    Add explore function
    Opens "debug.jGIS" with a pre-loaded basemap and the passed geoJSON
    file.

---
+++ python/jupytergis_lab/jupytergis_lab/explore.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from sidecar import Sidecar
+
+from jupytergis_lab import GISDocument
+
+
+def explore(geojson_path: str | Path) -> None:
+    """Run a JupyterGIS data interaction interface alongside a Notebook.
+
+    :param geojson_path: Path to a GeoJSON file.
+    """
+    doc = GISDocument("debug.jGIS")
+
+    # TODO: Basemap choices
+    doc.add_raster_layer(
+        "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
+    )
+
+    # TODO: Support lots of file types, and support Python objects like geodataframes.
+    doc.add_geojson_layer(str(geojson_path))
+
+    # TODO: Zoom to layer; is that feasible to do from Python? Currently not exposed in
+    #       Python API.
+    # FIXME: The document opens as intended, but the file has no contents and any
+    #        updates performed result in no update to the file.
